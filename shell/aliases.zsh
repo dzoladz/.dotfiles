472e97ac4353e95ae361cb47c8f5cfafa0d75a34
Derek Zoladz <derek@derekzoladz.com>
2023-01-15 22:43:45 -0500

    ADD: alias for podman-compose

--- shell/aliases.zsh
+++ shell/aliases.zsh
@@ -9,6 +9,7 @@
 
 alias brave='open -a /Applications/Brave\ Browser.app'
 alias docker=podman
+alias docker-compose=podman-compose
 alias ls='exa --long --header --links  --inode --blocks --accessed --modified --created --group --all --sort=.name  --group-directories-first'
 alias youtube-dl="youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4'"
 alias sadmin-opal='$HOME/.dotfiles/expect/opal-sierra-connection'
