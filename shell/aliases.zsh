30b7f0902d10743335ff626dc2907f7983e24fa0
Derek Zoladz <derek@derekzoladz.com>
2023-02-02 11:33:25 -0500

    UPDATE: add aliases for describing disk usage per directory

--- shell/aliases.zsh
+++ shell/aliases.zsh
@@ -10,7 +10,11 @@
 alias brave='open -a /Applications/Brave\ Browser.app'
 alias docker=podman
 alias docker-compose=podman-compose
-alias ls='exa --long --header --links  --inode --blocks --accessed --modified --created --group --all --sort=.name  --group-directories-first'
 alias youtube-dl="youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4'"
 alias sadmin-opal='$HOME/.dotfiles/expect/opal-sierra-connection'
 alias top='htop'
+
+# Customized command options, end in a 'z'
+alias dfz='du -sh * 2> /dev/null'
+alias duz='du -sh * 2> /dev/null'
+alias lsz='exa --long --header --links  --inode --blocks --accessed --modified --created --group --all --sort=.name  --group-directories-first'
