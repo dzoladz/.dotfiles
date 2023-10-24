4e1cb0893f9d06c4d26dfcc86f6c42817502c36d
Derek Zoladz <derek@derekzoladz.com>
2023-10-24 10:59:52 -0400

    ADD: PHP Xdebugger

--- php/setup_php.sh
+++ php/setup_php.sh
@@ -31,6 +31,7 @@ else
 fi
 
 # ----------------------------------------------------------------------
+# ----------------------------------------------------------------------
 # By default, brew will not permit the installation of
 # unsupported versions of PHP. To force the ability to
 # work with older version, tap shivammathur/php
@@ -38,16 +39,21 @@ brew tap shivammathur/php
 
 # Unsupported versions to install
 versions=(
+    '5.4'
     '7.2'
 )
 
-for version in ${versions[@]}; do
+for version in "${versions[@]}"; do
     brew install shivammathur/php/php@$version
 done
 
 # To activate and unsupported version of PHP, it must be linked:
 # brew unlink php  &&  brew link php@7.2
 # ----------------------------------------------------------------------
+# ----------------------------------------------------------------------
 
 # Install PHP package manager
 brew install composer
+
+# Install XDebug from https://pecl.php.net/ (PECL :: The PHP Extension Community Library)
+pecl install xdebug
